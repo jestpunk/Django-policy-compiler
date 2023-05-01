64d1f980348d13e39720322d8132d1e37e6f1427
Nikita Vasiliev <nikita.vasiliev@math.msu.ru>
2023-05-01 11:39:45 +0300

    environ + *pyc gitignore

--- istinaproject/istinaproject/settings.py
+++ istinaproject/istinaproject/settings.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-v)k%8j#u9e(ca(=z_+$)7_lfuk2)fq76z0)pln3*h%+*@0dm'
+# Безопасное прокидывание ключа и флага дебага
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
