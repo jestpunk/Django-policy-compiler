24151801f3ca4379da72462e2523922b4b783f1d
Nikita Vasiliev <nikita.vasiliev@math.msu.ru>
2023-05-01 11:06:18 +0300

    initial commit

---
+++ istinaproject/istinaproject/__init__.py
@@ -0,0 +1,1 @@
+# Application which automatically checks whether user of some system have permission to change, see, delete etc. some object or not. I use here ChRelBAC model of access in order to deploy this repo to https://istina.msu.ru in future
