27a536a43bf05908270ce4e83822c30f1eded16f
Nikita Vasiliev <nikita.vasiliev@math.msu.ru>
2023-05-02 10:55:35 +0300

    Created basic models

--- istinaproject/compiler/admin.py
+++ istinaproject/compiler/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Paper, Department
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Paper)
+admin.site.register(Department)
+
--- istinaproject/compiler/models.py
+++ istinaproject/compiler/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField('Имя пользователя', max_length=50)
+    user_department = models.ForeignKey('Department', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'[USER] {self.user_name} (dep: {self.user_department})'
+
+
+class Paper(models.Model):
+    paper_name = models.CharField('Название статьи', max_length=50)
+    paper_text = models.TextField('Текст статьи')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return f'[PAPER]: {self.paper_name}'
+
+class Department(models.Model):
+    department_name = models.CharField('Название отдела', max_length=50)
+
+
+    class Meta:
+        verbose_name = 'Отдел'
+        verbose_name_plural = 'Отделы'
+
+    def __str__(self):
+        return f'[DEPARTMENT] {self.department_name}'
