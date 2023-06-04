80cd9cac8fc6c90e2038ad684a10b84333534dd9
Nikita Vasiliev <nikita.vasiliev@math.msu.ru>
2023-06-04 22:33:33 +0300

    that's all for now

--- istinaproject/compiler/utils/compiler_function_test.py
+++ istinaproject/compiler/utils/compiler_function_test.py
@@ -7,69 +7,61 @@ from ..models import User, Paper, Department
 from .conftest import django_db_setup
 
 
-@pytest.mark.django_db
-def test_empty_source(django_db_setup):
-    source = User.objects.get(user_name="Пустой")
-    dest = Department.objects.get(department_name="Мехмат")
-
+@pytest.fixture
+def good_policy():
     r = Rule(
         Rule_category.ALLOWED,
         ["edit", "create"],
         "person to department, which is representative for his paper",
-        ("papers_of_user", "Paper_for_representative_department"),
+        ("papers_of_user", "paper_for_representative_department"),
     )
 
     p = Policy()
     p.add_rule(r)
+    return p
+
+
+@pytest.fixture
+def bad_policy():
+    r = Rule(
+        Rule_category.ALLOWED,
+        ["edit", "create"],
+        "it shouldn't work",
+        ("BLABLABLA", "BLABLABLABLABLABLA", "BLABLA"),
+    )
+
+    p = Policy()
+    p.add_rule(r)
+    return p
+
+
+@pytest.mark.django_db
+def test_empty_source(django_db_setup, good_policy):
+    source = User.objects.get(user_name="Пустой")
+    dest = Department.objects.get(department_name="Мехмат")
 
     result = True
-    result = compiler_function(source, "edit", dest, p)
-    # print(result)
+    result = compiler_function(source, "edit", dest, good_policy)
 
     assert result == False
 
 
 @pytest.mark.django_db
-def test_full_source(django_db_setup):
+def test_full_source(django_db_setup, good_policy):
     source = User.objects.get(user_name="Полный")
     dest = Department.objects.get(department_name="Мехмат")
 
-    r = Rule(
-        Rule_category.ALLOWED,
-        ["edit", "create"],
-        "person to department, which is representative for his paper",
-        ("papers_of_user", "Paper_for_representative_department"),
-    )
-
-    p = Policy()
-    p.add_rule(r)
-
     result = True
-    result = compiler_function(source, "edit", dest, p)
-    # print(result)
+    result = compiler_function(source, "edit", dest, good_policy)
 
     assert result == True
 
 
 @pytest.mark.django_db
-def test_wrong_field(django_db_setup):
+def test_wrong_field(django_db_setup, bad_policy):
     source = User.objects.get(user_name="Полный")
     dest = Department.objects.get(department_name="Мехмат")
 
-    r = Rule(
-        Rule_category.ALLOWED,
-        ["edit", "create"],
-        "person to department, which is representative for his paper",
-        ("BLABLABLA", "BLABLABLA"),
-    )
-
-    p = Policy()
-    p.add_rule(r)
-
     result = True
     with pytest.raises(FieldError):
-        result = compiler_function(source, "edit", dest, p)
-    # print(result)
-
-
-# assert result == True
+        result = compiler_function(source, "edit", dest, bad_policy)
